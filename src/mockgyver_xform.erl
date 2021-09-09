8cf4620a9fddaa2f59548ef214672930db839eb2
Tomas Abrahamsson <tomas.abrahamsson@gmail.com>
2021-09-09 18:20:48 +0200

    Rewrite ?MOCK to use ?MOCK_SESSION_PARAMS
    Drop the now-redundant processing of a wrapped m_init
    from the parse transform.

--- src/mockgyver_xform.erl
+++ src/mockgyver_xform.erl
@@ -52,7 +52,6 @@
 
 %% Records
 -record(m_session_params, {loc}).
--record(m_init, {exec_fun, loc}).
 -record(m_when, {m, f, a, action, loc}).
 -record(m_verify, {m, f, a, g, crit, loc}).
 
@@ -74,9 +73,8 @@ parse_transform_2(Forms0, Ctxt) ->
     Env = #env{mock_mfas  = find_mfas_to_mock(Forms0, Ctxt),
                trace_mfas = find_mfas_to_trace(Forms0, Ctxt)},
     {Forms1, _} = rewrite_session_params_stmts(Forms0, Ctxt, Env),
-    {Forms2, _} = rewrite_init_stmts(Forms1, Ctxt, Env),
-    {Forms3, _} = rewrite_when_stmts(Forms2, Ctxt),
-    {Forms, _}  = rewrite_verify_stmts(Forms3, Ctxt),
+    {Forms2, _} = rewrite_when_stmts(Forms1, Ctxt),
+    {Forms, _}  = rewrite_verify_stmts(Forms2, Ctxt),
     parse_trans:revert(Forms).
 
 %%------------------------------------------------------------
@@ -95,30 +93,6 @@ rewrite_session_params_stmts_2(Type, Form0, _Ctxt, Env) ->
             [Form] = codegen:exprs(
                        fun() ->
                                {{'$var',  MockMfas}, {'$var',  TraceMfas}}
-                       end),
-            Afters = [],
-            {Befores, Form, Afters, false, Env};
-        _ ->
-            {Form0, true, Env}
-    end.
-
-%%------------------------------------------------------------
-%% init statements
-%%------------------------------------------------------------
-rewrite_init_stmts(Forms, Ctxt, Env) ->
-    parse_trans:do_transform(fun rewrite_init_stmts_2/4, Env, Forms, Ctxt).
-
-rewrite_init_stmts_2(Type, Form0, _Ctxt, Env) ->
-    case is_mock_expr(Type, Form0) of
-        {true, #m_init{exec_fun=ExecFun}} ->
-            Befores = [],
-            MockMfas = Env#env.mock_mfas,
-            TraceMfas = Env#env.trace_mfas,
-            [Form] = codegen:exprs(
-                       fun() ->
-                               mockgyver:exec({{'$var',  MockMfas},
-                                               {'$var',  TraceMfas}},
-                                              {'$form', ExecFun})
                        end),
             Afters = [],
             {Befores, Form, Afters, false, Env};
@@ -225,16 +199,12 @@ is_mock_expr(_Type, _Form) ->
 analyze_mock_form(Info, Location) ->
     case Info of
         {m_session_params, []} -> analyze_session_params_expr(Location);
-        {m_init, [Expr]}       -> analyze_init_expr(Expr, Location);
         {m_when, [Expr]}       -> analyze_when_expr(Expr, Location);
         {m_verify, [Expr]}     -> analyze_verify_expr(Expr, Location)
     end.
 
 analyze_session_params_expr(Location) ->
     #m_session_params{loc=Location}.
-
-analyze_init_expr(Expr, Location) ->
-    #m_init{exec_fun=Expr, loc=Location}.
 
 analyze_when_expr(Expr, Location) ->
     %% The sole purpose of the entire if expression is to let us write
